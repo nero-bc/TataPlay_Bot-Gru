fc4094c7fa67240b909517dd236902453d899638
nero-bc <126649764+nero-bc@users.noreply.github.com>
2023-07-28 12:12:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-api_id = "28587040"
+API_ID = "28587040"
 
-api_hash = "20f4a4a125d663eb14693cf716788400"
+API_HASH = "20f4a4a125d663eb14693cf716788400"
 
-bot_token = "6338736604:AAEzmj_3MegOh_QIAzoskhmZVa7d3DlqmRk"
+BOT_TOKEN = "6338736604:AAEzmj_3MegOh_QIAzoskhmZVa7d3DlqmRk"
 
 GROUP_TAG = "RC"
 
-sudo_users = [1988545170]
+SUDO_USERS = [1988545170]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
